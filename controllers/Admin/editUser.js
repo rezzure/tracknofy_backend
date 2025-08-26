4ee9273e91f9b397be55cc1fcd6a8565100706ea
Ankur Shrivastav <tinkushrivastav78@gmail.com>
2025-08-26 16:25:15 +0530

    update in editUser

--- controllers/Admin/editUser.js
+++ controllers/Admin/editUser.js
@@ -15,6 +15,49 @@ const editUser = async(req,res)=>{
         message:"user not found"
       })
     }
+    if(user1.role === "admin"){
+      const admin = await Admin.findOne({email:user1.email})
+      console.log(admin)
+      if(name) admin.name=name
+      if(email) admin.email = email
+      if(mobile) admin.mobile = mobile
+      if(password){
+      let salt = await bcrypt.genSalt(10);
+      let hashPassword = await bcrypt.hash(password, salt);
+      admin.password=hashPassword
+      
+    }
+    await admin.save()
+    }
+    if(user1.role === "client"){
+      const client = await Client.findOne({email:user1.email})
+      console.log(client)
+      if(name) client.name=name
+      if(email) client.email = email
+      if(mobile) client.mobile = mobile
+      if(password){
+      let salt = await bcrypt.genSalt(10);
+      let hashPassword = await bcrypt.hash(password, salt);
+      client.password=hashPassword
+      }
+      await client.save()
+    }
+
+    if(user1.role === "supervisor"){
+      const supervisor = await Supervisor.findOne({email:user1.email})
+      console.log(supervisor)
+      if(name) supervisor.name=name
+      if(email) supervisor.email = email
+      if(mobile) supervisor.mobile = mobile
+      if(password){
+      let salt = await bcrypt.genSalt(10);
+      let hashPassword = await bcrypt.hash(password, salt);
+      supervisor.password=hashPassword
+      }
+      await supervisor.save()
+    }
+
+
     if(name) user1.name=name
     if(email) user1.email=email
     if(mobile) user1.mobile=mobile

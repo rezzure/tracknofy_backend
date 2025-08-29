d7721cd787ef66cd710f8b2f5b1fe4f55238a13e
Ankur Shrivastav <tinkushrivastav78@gmail.com>
2025-08-29 10:32:01 +0530

    minor change

--- controllers/Admin/deleteVendorDetail.js
+++ controllers/Admin/deleteVendorDetail.js
@@ -2,21 +2,22 @@ const VendorManagement = require("../../Schema/vendorManagement.schema/vendorMan
 
 const deleteVendorDetail = async (req, res) =>{
     const {_id} = req.params
-
+    console.log(_id)
     try {
         const deletedData = await VendorManagement.findByIdAndDelete(_id)
         if(!deletedData) {
             return res.status(500).send({
                 success: false,
-                message: "Vendor data not found"
+                message: "Vendor Data Not Found"
             })
         }
         return res.status(200).send({
             success: true,
-            message: "Vendor data deleted successfully",
+            message: "Vendor Data Deleted Successfully",
             data: deletedData
         })
     } catch (error) {
+        console.log(error.message)
         return res.status(404).send({
             success: false,
             message: `Internal server error ${error.message}`

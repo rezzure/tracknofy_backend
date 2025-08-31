17ba86ea9aa72d4f6bb7347ef2dbfaf0f1b85463
rezzure <rezzure@users.noreply.github.com>
2025-08-31 12:32:46 +0530

    Added Master Data config schema

---
+++ Schema/AddDataDictionary.schema/MasterItem.model.js
@@ -0,0 +1,45 @@
+import mongoose from "mongoose";
+
+const MasterItemSchema = new mongoose.Schema(
+  {
+    master_type: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "MasterConfigSchema", // reference to master_type
+      required: true,
+    },
+    master_item_id: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    master_item_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    master_item_description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+
+    created_by: {
+      type: String,
+      required: true,
+    },
+    updated_by: {
+      type: String,
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+    collection: "master_items",
+  }
+);
+
+export default mongoose.model("MasterItem", MasterItemSchema);
---
+++ Schema/addMasterType.schema/addMasterType.model.js
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const MasterConfigSchema = new mongoose.Schema(
+  {
+    master_type_id: {
+      type: String, // You can also use ObjectId if needed
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    master_type_name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    master_type_description: {
+      type: String,
+      default: "",
+      trim: true,
+    },
+    created_by: {
+      type: String, // store user_id or username
+      required: true,
+    },
+    updated_by: {
+      type: String, // store user_id or username
+    },
+    is_active: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  {
+    timestamps: { createdAt: "created_at", updatedAt: "updated_at" }, // auto manages created_at & updated_at
+    collection: "master_configurations", // collection name
+  }
+);
+
+export default mongoose.model("MasterTypeConfig", MasterConfigSchema);

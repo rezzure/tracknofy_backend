f535578e7571447513effaa96d336389aa9686c7
Ankur Shrivastav <tinkushrivastav78@gmail.com>
2025-09-15 18:09:28 +0530

    minor change in api

--- router/kanbanRouter/kanBanRouter.js
+++ router/kanbanRouter/kanBanRouter.js
@@ -1,41 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const taskController = require('../../controllers/kanbanTaskController/taskController');
-
-// // Get all tasks
-// router.get('/get/tasks', taskController.getAllTasks);
-
-// // Get tasks by status
-// router.get('/get/tasks/status/:status', taskController.getTasksByStatus);
-
-// // Get task by ID
-// router.get('/get/task/:id', taskController.getTaskById);
-
-// // Create a new task
-// router.post('/create/task', taskController.createTask);
-
-// // Update a task
-// router.put('/update/task:id', taskController.updateTask);
-
-// // Update task status
-// router.patch('/update/task/status/:id/status', taskController.updateTaskStatus);
-
-// // Delete a task
-// router.delete('/delete/task/:id', taskController.deleteTask);
-
-// // Get tasks statistics
-// router.get('/get/stats/summary', taskController.getTasksStatistics);
-
-// module.exports = router;
-
-
-
-
-
-
-
-
-
 const express = require('express');
 const router = express.Router();
 const taskController = require('../../controllers/kanbanTaskController/taskController');
@@ -73,7 +35,7 @@ router.get('/get/stats/site/:siteId', taskController.getSiteTaskStatistics);
 // task movement routers
 
 // Get all movement history for a specific task
-router.get('/task-movements/:taskId',verification,taskMovement.getMovementById)
+router.get('/get/task-movements/:taskId',verification,taskMovement.getMovementById)
 
 // Get movement history for a specific site
 router.get('/site-movements/:siteId',verification,taskMovement.getMovementBySiteId)

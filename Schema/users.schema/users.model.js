6d04e879c322baf0e214ad3972cc63b295d74a8e
Rohit Soni <165762834+Rohits906@users.noreply.github.com>
2025-10-06 14:05:54 +0530

    Designer role created

--- Schema/users.schema/users.model.js
+++ Schema/users.schema/users.model.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
   },
   role: {
     type: String,
-    enum: ['admin', 'supervisor', 'client'],
+    enum: ['admin', 'supervisor', 'client', 'designer'],
     default: 'client',
     required: true
   },

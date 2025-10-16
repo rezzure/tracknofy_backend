e2f080e9e56efdf3322b015daeab762a551c918e
Akash Chauhan <121815603+akashchauhan555@users.noreply.github.com>
2025-10-16 18:40:18 +0530

    made schema and controller for my form

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const dashboardRoutes = require('./router/dashbordRouter/dashboardRouter.js');
 const leadRoutes = require("./router/crm.router/crmRouter.js")
 const crmRoutes  = require("./router/crmTaskManager.router/crmTaskManager.js")
 const clientFinancialDetail  = require("./router/clientFinancialDetail.router/clientFinancialDetail.router.js")
+const myFormRoutes   = require("./router/myForm.router/myFormRouter.js")
 const fs = require('fs');
 
 connectDB();
@@ -41,6 +42,7 @@ app.use('/api', formRoutes);
 app.use('/api', leadRoutes);
 app.use('/api', crmRoutes);
 app.use('/api', clientFinancialDetail);
+app.use('/api', myFormRoutes);
 // app.use('/api', submissionRoutes);
 app.use('/api', fileRoutes);
 app.use('/api/dashboard', dashboardRoutes);
